c529250747437baf9a8015c5737b41c82302a506
DreamPerson <dreamPerson0106@gmail.com>
2023-08-21 12:28:02 +0900

    test trace

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import {ethers} from "ethers";
 import { wssProvider } from "./src/constants.js";
 
-const analyzeTransaction = async (tx) => {
-
+const analyzeTransaction = async (tx_hash) => {
+    const tx_trace = await provider.send('debug_traceTransaction', transactionHash);
+    console.log(tx_trace);
 }
 
 const main = async () => {
     console.log("Start analyzing transaction gas fees");
     await analyzeTransaction("0xbae1881421d5d5c598a3762465a07211e65856a41f97c8906846a5f59e0158da");
-    await analyzeTransaction("0x3820844d43995e3b7d64d9cad20b06a9855d23bfd3e242c0d4a89f9e6dbab069");
-    await analyzeTransaction("0x8fe6b7b191759459bf302b2c7cc0a67e6e6ad31db26263afece3bc3e81c43f5f");
+    // await analyzeTransaction("0x3820844d43995e3b7d64d9cad20b06a9855d23bfd3e242c0d4a89f9e6dbab069");
+    // await analyzeTransaction("0x8fe6b7b191759459bf302b2c7cc0a67e6e6ad31db26263afece3bc3e81c43f5f");
 }
 
 main()
