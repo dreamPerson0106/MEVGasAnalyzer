52ac5e63e9cf2f87dc09677e3b61ce33dc7a6bee
DreamPerson <dreamPerson0106@gmail.com>
2023-08-21 15:17:50 +0900

    asdf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const analyzeTransaction = async (tx_hash) => {
 
 const main = async () => {
     console.log("Start analyzing transaction gas fees");
-    await analyzeTransaction("0x688108bf5dd64b3289436950d95053b43efb83e0bb5fd79ed82d67fd3875c905");
+    await analyzeTransaction("0xb9a24ad80c61da710e10194ccb11ff4c549a7d9b246456474cbab62dcf132d1e");
     // await analyzeTransaction("0x3820844d43995e3b7d64d9cad20b06a9855d23bfd3e242c0d4a89f9e6dbab069");
     // await analyzeTransaction("0x8fe6b7b191759459bf302b2c7cc0a67e6e6ad31db26263afece3bc3e81c43f5f");
 }
