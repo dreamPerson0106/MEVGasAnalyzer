3db54c678a4421c21c765ab74f7f69bdca6c6654
DreamPerson <dreamPerson0106@gmail.com>
2023-08-21 21:06:26 +0900

    start detect fees

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 import {ethers} from "ethers";
 import { provider, wssProvider } from "./src/constants.js";
 
-const analyzeTransaction = async (tx_hash) => {
+const analyzeTransaction = async (tx) => {
+    const tx_hash = tx.hash;
     const txReceipt = await provider.getTransactionReceipt(tx_hash);
     const confirmedBlock = await provider.getBlock(txReceipt.blockNumber);
     const validator = confirmedBlock.miner;
 
+    // Start detect fee using transfer to miner
     const tx_trace = await provider.send('debug_traceTransaction', [tx_hash,{"tracer": "callTracer"}]);
     const calls = tx_trace.calls;
     for(let i = 0 ; i < calls.length ; ++ i) {
         const to = calls[i].to;
         if(to.toLowerCase() == validator.toLowerCase()) {
-            console.log("We detect transfer to miner.");
             const valueToTransfer = ethers.BigNumber.from(calls[i].value);
-            console.log(ethers.utils.formatEther(valueToTransfer));
+            console.log(tx_hash, "Send fee using transfer",ethers.utils.formatEther(valueToTransfer), "eth");
         }
+    }
+
+    // Start detect fee using gwei to miner
+    if(txReceipt.type == 0) {
+        const priorityGwei = tx.gasPrice.sub(confirmedBlock.baseFeePerGas);
+        const gasUsedForMiner = priorityGwei.mul(txReceipt.gasUsed);
+        console.log(tx_hash, "Send fee using type 0",ethers.utils.formatUnits(valueToTransfer, "gwei"), "Gwei");
+    }
+    if(txReceipt.type == 2) {
+        const priorityGwei = tx.maxFeePerGas.sub(confirmedBlock.baseFeePerGas).gt(tx.maxPriorityFeePerGas) ? tx.maxPriorityFeePerGas : tx.maxFeePerGas.sub(confirmedBlock.baseFeePerGas);
+        const gasUsedForMiner = priorityGwei.mul(txReceipt.gasUsed);
+        console.log(tx_hash, "Send fee using type 2",ethers.utils.formatUnits(valueToTransfer, "gwei"), "Gwei");
     }
 }
 
 const main = async () => {
     console.log("Start analyzing transaction gas fees");
-    await analyzeTransaction("0xb9a24ad80c61da710e10194ccb11ff4c549a7d9b246456474cbab62dcf132d1e");
-    // await analyzeTransaction("0x3820844d43995e3b7d64d9cad20b06a9855d23bfd3e242c0d4a89f9e6dbab069");
-    // await analyzeTransaction("0x8fe6b7b191759459bf302b2c7cc0a67e6e6ad31db26263afece3bc3e81c43f5f");
+    wssProvider.on("block", async (blk) => {
+        const txs = (await wssProvider.getBlockWithTransactions(blk)).transactions;
+        for(let i = 0 ; i < txs.length ; ++ i) {
+            if(txs[i].to.toLowerCase() === "0x58dF81bAbDF15276E761808E872a3838CbeCbcf9".toLowerCase()) {
+                analyzeTransaction(txs[i]);
+            }
+        }
+    })
 }
 
 main()
