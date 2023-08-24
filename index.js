270042784147144fc99352fdcf8118cb186f263e
DreamPerson <dreamPerson0106@gmail.com>
2023-08-25 00:23:45 +0900

    asdf

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import { ethers } from "ethers";
 import { provider, wssProvider } from "./src/constants.js";
 
 let mempoolTxs = [];
-
+let latestBlockNumber = 0;
 const analyzeTransaction = async (tx) => {
   const tx_hash = tx.hash;
   const txReceipt = await provider.getTransactionReceipt(tx_hash);
   const confirmedBlock = await provider.getBlock(txReceipt.blockNumber);
   const validator = confirmedBlock.miner;
 
-  console.log(tx.hash, txReceipt.status != 0 ? "Success" : "Failed");
+//   console.log(tx.hash, txReceipt.status != 0 ? "Success" : "Failed");
 
   // Start detect fee using transfer to miner
   const tx_trace = await provider.send("debug_traceTransaction", [
@@ -25,11 +25,11 @@ const analyzeTransaction = async (tx) => {
     if (to.toLowerCase() == validator.toLowerCase()) {
       const valueToTransfer = ethers.BigNumber.from(calls[i].value);
       feeInEther = feeInEther.add(valueToTransfer);
-      console.log(
-        "Send fee using transfer",
-        ethers.utils.formatEther(valueToTransfer),
-        "eth"
-      );
+    //   console.log(
+    //     "Send fee using transfer",
+    //     ethers.utils.formatEther(valueToTransfer),
+    //     "eth"
+    //   );
     }
   }
 
@@ -38,11 +38,11 @@ const analyzeTransaction = async (tx) => {
     const priorityGwei = tx.gasPrice.sub(confirmedBlock.baseFeePerGas);
     const gasUsedForMiner = priorityGwei.mul(txReceipt.gasUsed);
     feeInGwei = feeInGwei.add(gasUsedForMiner);
-    console.log(
-      "Send fee using type 0",
-      ethers.utils.formatEther(gasUsedForMiner),
-      "eth"
-    );
+    // console.log(
+    //   "Send fee using type 0",
+    //   ethers.utils.formatEther(gasUsedForMiner),
+    //   "eth"
+    // );
   }
   if (txReceipt.type == 2) {
     const priorityGwei = tx.maxFeePerGas
@@ -52,29 +52,32 @@ const analyzeTransaction = async (tx) => {
       : tx.maxFeePerGas.sub(confirmedBlock.baseFeePerGas);
     const gasUsedForMiner = priorityGwei.mul(txReceipt.gasUsed);
     feeInGwei = feeInGwei.add(gasUsedForMiner);
-    console.log(
-      "Send fee using type 2",
-      ethers.utils.formatEther(gasUsedForMiner),
-      "eth"
-    );
+    // console.log(
+    //   "Send fee using type 2",
+    //   ethers.utils.formatEther(gasUsedForMiner),
+    //   "eth"
+    // );
   }
 
-  console.log(
-    "Tx hash:",
-    tx_hash,
-    "Success?:",
-    txReceipt.status != 0 ? "✔" : "X",
-    "fee in Ether:",
-    ethers.utils.parseEther(feeInEther.toString()),
-    "fee in Gwei:",
-    ethers.utils.parseEther(feeInGwei.toString())
-  );
+  if(latestBlockNumber != 0) {
+    console.log(
+        "Tx hash:",
+        tx_hash,
+        "Success?:",
+        txReceipt.status != 0 ? "✔" : "X",
+        "fee in Ether:",
+        ethers.utils.formatEther(feeInEther),
+        "fee in Gwei:",
+        ethers.utils.formatEther(feeInGwei)
+    );
+  }
 };
 
 const main = async () => {
   console.log("Start analyzing transaction gas fees");
   wssProvider.on("block", async (blk) => {
     console.log(blk);
+    latestBlockNumber = blk;
     const txs = (await wssProvider.getBlockWithTransactions(blk)).transactions;
     for (let i = 0; i < txs.length; ++i) {
       const indexOfHashInMempool = mempoolTxs.indexOf(txs[i].hash);
