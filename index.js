ffef592bfe14ebcbe56c9cd870cfe9ae0d53b136
DreamPerson <dreamPerson0106@gmail.com>
2023-08-25 16:42:59 +0900

    asdf

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import { ethers } from "ethers";
 import { provider, wssProvider } from "./src/constants.js";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 const jsdom = require("jsdom");
-const https = require('https');
-const http = require('http');
-const cheerio = require('cheerio');
+const https = require("https");
+const http = require("http");
+const cheerio = require("cheerio");
 
-const puppeteer = require('puppeteer');
-
+const puppeteer = require("puppeteer-extra");
+const { executablePath } = require("puppeteer");
 
 let mempoolTxs = [];
 
@@ -27,43 +27,48 @@ const analyzeTransaction = async (tx) => {
   const url = `https://etherscan.io/tx/${tx_hash}`;
 
   const options = {
-    host: 'etherscan.io',
-      port: '80',
-      path: `/tx/${tx_hash}`,
-      method: 'GET',
-      headers: {
-        'Cookie': 'displaymode=dark; etherscan_address_format=0; etherscan_datetime_format=UTC; etherscan_settings=x0:1|x1:1|x2:en|x3:USD|x4:0|x5:0|x6:ENS|x7:UTC|x8:1; CultureInfo=en; __stripe_mid=9b401b31-27de-44b2-ae36-6bc1a958f27848743e; etherscan_cookieconsent=True; bitmedia_fid=eyJmaWQiOiIwYjJjNjE5YzhlOTEwYjI4MGQxNjBkYWMzMTdmNWVhOSIsImZpZG5vdWEiOiJmMWRjNzBiZDUwMWFmMzBiMDIyODQ0MTZlNjM5MzE2YiJ9; _ga=GA1.1.70512955.1690950419; ASP.NET_SessionId=adkmgfe1dgr3h3htzugswyw3; etherscan_pwd=4792:Qdxb:29xiH0wXNeFfqV+qt5UAUIdMf6Faa9cmky4Z2a45KUQ=; etherscan_userid=DreamPerson0106; etherscan_autologin=True; __cflb=0H28vPcoRrcznZcNZSuFrvaNdHwh857bnso7roPCUmi; cf_clearance=vznZeZFiNGF43TzLmEy7YuXuUnqw44c8d92aXi0LBDM-1692943299-0-1-ba6f4700.1bb4a04c.3a83a946-0.2.1692943299; _ga_NHZNQE2B8K=GS1.1.1692943357.25.1.1692943363.0.0.0; _ga_T1JC9RNQXV=GS1.1.1692943367.107.1.1692945750.0.0.0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-  }
+    host: "etherscan.io",
+    port: "80",
+    path: `/tx/${tx_hash}`,
+    method: "GET",
+    headers: {
+      Cookie:
+        "displaymode=dark; etherscan_address_format=0; etherscan_datetime_format=UTC; etherscan_settings=x0:1|x1:1|x2:en|x3:USD|x4:0|x5:0|x6:ENS|x7:UTC|x8:1; CultureInfo=en; __stripe_mid=9b401b31-27de-44b2-ae36-6bc1a958f27848743e; etherscan_cookieconsent=True; bitmedia_fid=eyJmaWQiOiIwYjJjNjE5YzhlOTEwYjI4MGQxNjBkYWMzMTdmNWVhOSIsImZpZG5vdWEiOiJmMWRjNzBiZDUwMWFmMzBiMDIyODQ0MTZlNjM5MzE2YiJ9; _ga=GA1.1.70512955.1690950419; ASP.NET_SessionId=adkmgfe1dgr3h3htzugswyw3; etherscan_pwd=4792:Qdxb:29xiH0wXNeFfqV+qt5UAUIdMf6Faa9cmky4Z2a45KUQ=; etherscan_userid=DreamPerson0106; etherscan_autologin=True; __cflb=0H28vPcoRrcznZcNZSuFrvaNdHwh857bnso7roPCUmi; cf_clearance=vznZeZFiNGF43TzLmEy7YuXuUnqw44c8d92aXi0LBDM-1692943299-0-1-ba6f4700.1bb4a04c.3a83a946-0.2.1692943299; _ga_NHZNQE2B8K=GS1.1.1692943357.25.1.1692943363.0.0.0; _ga_T1JC9RNQXV=GS1.1.1692943367.107.1.1692945750.0.0.0",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
 
-  var req = http.request(options, response => {
-    let data = '';
-  
-    response.on('data', chunk => {
-      data += chunk;
-    });
-  
-    response.on('end', () => {
-      console.log("data=", data);
-      const $ = cheerio.load(data);
-      const siteComponent = $('#ContentPlaceHolder1_divTimeStamp  >  div > div:last-child > span:last-child');
-      console.log(siteComponent.length);
-    });
-  }).on('error', error => {
+  var req = http
+    .request(options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        console.log("data=", data);
+        const $ = cheerio.load(data);
+        const siteComponent = $(
+          "#ContentPlaceHolder1_divTimeStamp  >  div > div:last-child > span:last-child"
+        );
+        console.log(siteComponent.length);
+      });
+    })
+    .on("error", (error) => {
       console.error(error);
     });
   req.end();
 
-
   // https.get(url, response => {
   //   let data = '';
-  
+
   //   response.on('data', chunk => {
   //     data += chunk;
   //   });
-  
+
   //   response.on('end', () => {
   //     console.log(data);
   //     const $ = cheerio.load(data);
@@ -138,22 +143,44 @@ const analyzeTransaction = async (tx) => {
 const main = async () => {
   console.log("Start analyzing MEV txs");
 
-  
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+  puppeteer.use(StealthPlugin());
+
+  puppeteer
+    .launch({ headless: false, executablePath: executablePath() })
+    .then(async (browser) => {
+      const page = await browser.newPage();
+      await page.goto(
+        "https://etherscan.io/tx/0x861cee51f6d858d149d1dfc02682db3c85e2775f937dfcb14c625e86777026ee"
+      );
+      await page.waitForTimeout(2000);
+      console.log(await page.content())
+      // await browser.close();
+    });
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setExtraHTTPHeaders({
-    'Cookie': 'displaymode=dark; etherscan_address_format=0; etherscan_datetime_format=UTC; etherscan_settings=x0:1|x1:1|x2:en|x3:USD|x4:0|x5:0|x6:ENS|x7:UTC|x8:1; CultureInfo=en; __stripe_mid=9b401b31-27de-44b2-ae36-6bc1a958f27848743e; etherscan_cookieconsent=True; bitmedia_fid=eyJmaWQiOiIwYjJjNjE5YzhlOTEwYjI4MGQxNjBkYWMzMTdmNWVhOSIsImZpZG5vdWEiOiJmMWRjNzBiZDUwMWFmMzBiMDIyODQ0MTZlNjM5MzE2YiJ9; _ga=GA1.1.70512955.1690950419; ASP.NET_SessionId=adkmgfe1dgr3h3htzugswyw3; etherscan_pwd=4792:Qdxb:29xiH0wXNeFfqV+qt5UAUIdMf6Faa9cmky4Z2a45KUQ=; etherscan_userid=DreamPerson0106; etherscan_autologin=True; cf_clearance=vznZeZFiNGF43TzLmEy7YuXuUnqw44c8d92aXi0LBDM-1692943299-0-1-ba6f4700.1bb4a04c.3a83a946-0.2.1692943299; _ga_NHZNQE2B8K=GS1.1.1692947380.26.0.1692947380.0.0.0; __cflb=0H28vPcoRrcznZcNZSuFrvaNdHwh858YTBXb4EFFFTz; _ga_T1JC9RNQXV=GS1.1.1692943367.107.1.1692947385.0.0.0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    Cookie:
+      "displaymode=dark; etherscan_address_format=0; etherscan_datetime_format=UTC; etherscan_settings=x0:1|x1:1|x2:en|x3:USD|x4:0|x5:0|x6:ENS|x7:UTC|x8:1; CultureInfo=en; __stripe_mid=9b401b31-27de-44b2-ae36-6bc1a958f27848743e; etherscan_cookieconsent=True; bitmedia_fid=eyJmaWQiOiIwYjJjNjE5YzhlOTEwYjI4MGQxNjBkYWMzMTdmNWVhOSIsImZpZG5vdWEiOiJmMWRjNzBiZDUwMWFmMzBiMDIyODQ0MTZlNjM5MzE2YiJ9; _ga=GA1.1.70512955.1690950419; ASP.NET_SessionId=adkmgfe1dgr3h3htzugswyw3; etherscan_pwd=4792:Qdxb:29xiH0wXNeFfqV+qt5UAUIdMf6Faa9cmky4Z2a45KUQ=; etherscan_userid=DreamPerson0106; etherscan_autologin=True; cf_clearance=vznZeZFiNGF43TzLmEy7YuXuUnqw44c8d92aXi0LBDM-1692943299-0-1-ba6f4700.1bb4a04c.3a83a946-0.2.1692943299; _ga_NHZNQE2B8K=GS1.1.1692947380.26.0.1692947380.0.0.0; __cflb=0H28vPcoRrcznZcNZSuFrvaNdHwh858YTBXb4EFFFTz; _ga_T1JC9RNQXV=GS1.1.1692943367.107.1.1692947385.0.0.0",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
   });
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0"
+  );
   await page.setJavaScriptEnabled(true);
 
-
-  const url = 'https://etherscan.io/tx/0x4a961ee2a00161a1c6d3d135885cf61ea6fde274e72f68798ed519efc2d36492';
-  await page.goto(url, { waitUntil: 'networkidle0' });
+  const url =
+    "https://etherscan.io/tx/0x4a961ee2a00161a1c6d3d135885cf61ea6fde274e72f68798ed519efc2d36492";
+  await page.goto(url, { waitUntil: "load" });
   console.log(await page.content());
-  const blockNumberElements = await page.$$('div.media-body span');
-  const blockNumberElement = blockNumberElements.find(element => element.textContent.includes('Block Height'));
+  const blockNumberElements = await page.$$("div.media-body span");
+  const blockNumberElement = blockNumberElements.find((element) =>
+    element.textContent.includes("Block Height")
+  );
   const blockNumber = blockNumberElement.nextElementSibling.textContent.trim();
   console.log(blockNumber);
 
