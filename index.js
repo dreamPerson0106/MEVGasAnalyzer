e9d9aafafabda83f676ba760da47263936f19fce
DreamPerson <dreamPerson0106@gmail.com>
2023-08-25 15:31:10 +0900

    asdf

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const analyzeTransaction = async (tx) => {
       method: 'GET',
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(post_data)
+          'Content-Type': 'application/x-www-form-urlencoded'
       }
   }
 
