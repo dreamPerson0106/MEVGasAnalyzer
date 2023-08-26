40746823b57fc14ea8ba8c4efcc8ebed0c2bb770
DreamPerson <dreamPerson0106@gmail.com>
2023-08-26 17:47:25 +0900

    asdf

--- index.js
+++ index.js
@@ -80,27 +80,29 @@ const analyzeTransaction = async (tx) => {
 const main = async () => {
   console.log("Start analyzing MEV txs");
 
-  wssProvider.on("block", async (blk) => {
-    await sleep(5000);
-    console.log(blk);
-    const txs = (await wssProvider.getBlockWithTransactions(blk)).transactions;
-    for (let i = 0; i < txs.length; ++i) {
-      const indexOfHashInMempool = mempoolTxs.indexOf(txs[i].hash);
-      if (txs[i].to != null && indexOfHashInMempool === -1) {
-        analyzeTransaction(txs[i]);
-      }
-      if (indexOfHashInMempool >= 0) {
-        mempoolTxs = mempoolTxs.slice(
-          indexOfHashInMempool,
-          indexOfHashInMempool
-        );
-      }
-    }
-  });
+  analyzeTransaction(await wssProvider.getTransaction("0x3c64cfc9ee797f98f99c217bed976b0d6e80e2f132345ecdeaa9db85497f72be"));
 
-  wssProvider.on("pending", async (hash) => {
-    if (mempoolTxs.indexOf(hash) === -1) mempoolTxs.push(hash);
-  });
+  // wssProvider.on("block", async (blk) => {
+  //   await sleep(5000);
+  //   console.log(blk);
+  //   const txs = (await wssProvider.getBlockWithTransactions(blk)).transactions;
+  //   for (let i = 0; i < txs.length; ++i) {
+  //     const indexOfHashInMempool = mempoolTxs.indexOf(txs[i].hash);
+  //     if (txs[i].to != null && indexOfHashInMempool === -1) {
+  //       analyzeTransaction(txs[i]);
+  //     }
+  //     if (indexOfHashInMempool >= 0) {
+  //       mempoolTxs = mempoolTxs.slice(
+  //         indexOfHashInMempool,
+  //         indexOfHashInMempool
+  //       );
+  //     }
+  //   }
+  // });
+
+  // wssProvider.on("pending", async (hash) => {
+  //   if (mempoolTxs.indexOf(hash) === -1) mempoolTxs.push(hash);
+  // });
 };
 
 main();
