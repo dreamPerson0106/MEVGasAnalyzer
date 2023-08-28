a48e57887cffe9bfc40dcac66c032067f8d72a88
DreamPerson <dreamPerson0106@gmail.com>
2023-08-29 02:15:36 +0900

    asdf

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const analyzeTransaction = async (tx) => {
   const validator = confirmedBlock.miner;
 
   const responseOfEtherscan = await axios.get(`http://65.109.109.169:5000/${tx_hash}`);
-  console.log(responseOfEtherscan);
   if(responseOfEtherscan.data.status == true) {
     console.log("This is not a MEV tx!");
     return;
